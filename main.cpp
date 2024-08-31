8c51678e1c726d7b13f534c599e751c31096c12d
fnlnandy <andyfaniloniaina01@gmail.com>
2024-08-31 21:24:08 +0300

    Modified main testing program.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <midcxxap/midcxxap.hpp>
 
 int main(int argc, char **argv)
 {
-    std::printf("Hello, World!\n");
-    
+    auto argumentParser = MidCxxAP::ArgumentParser("testmidcxxap", "test program for the midcxxap library.");
+
+    argumentParser.addArgument("filename");
+    argumentParser.addArgument("-s", "--source", true);
+    argumentParser.addArgument("-d", "--destination", true);
+
+    auto parsedArgs = argumentParser.parseArgs(argc - 1, argv + 1);
+
+    std::printf("FILENAME: %s SOURCE: %s DESTINATION: %s\n", parsedArgs["filename"].c_str(), parsedArgs["source"].c_str(), parsedArgs["destination"].c_str());
     return 0;
 }
